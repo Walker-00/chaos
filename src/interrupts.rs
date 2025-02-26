e8db98331a236d91bdf134d6d4f045b07e95a8a4
Linus Walker <rissk_it@proton.me>
2025-02-26 19:30:28 +0630

    chore: shits

---
+++ src/interrupts.rs
@@ -0,0 +1,52 @@
+use lazy_static::lazy_static;
+use pic8259::ChainedPics;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use crate::{print, println};
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
+        idt
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 #![no_std]
 #![no_main]
 
 // extern crate panic_halt;
 extern crate rlibc;
 
+mod interrupts;
 mod vga;
+
 use core::panic::PanicInfo;
 
-use x86_64::instructions::hlt;
+use bootloader_api::BootInfo;
+use x86_64::{instructions::hlt, structures::gdt};
+
+pub fn init() {
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
@@ -34,8 +45,20 @@ fn hlt_loop() -> ! {
 }
 
 /// The kernel’s main entry point.
-#[unsafe(no_mangle)]
-pub extern "C" fn kernel_start() -> ! {
-    println!("Poop");
+// #[unsafe(no_mangle)]
+pub fn kernel_start(boot_info: &'static mut BootInfo) -> ! {
+    println!("Chaos Cha-OS");
+    #[cfg(test)]
+    test_main();
+
     hlt_loop()
+}
+
+bootloader_api::entry_point!(kernel_start);
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
